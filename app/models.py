847ebc2ac1d51bd4eac4c97e77c1f827f0808b23
DUDU1608 <krish.alok@gmail.com>
2025-09-02 19:45:16 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -131,6 +131,7 @@ class StockExit(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     date = db.Column(db.Date, nullable=False)
+    rst_no = db.Column(db.String(50), index=True)
     warehouse = db.Column(db.String(100), nullable=False)
     stockist_name = db.Column(db.String(100), nullable=False)
     mobile = db.Column(db.String(20))
